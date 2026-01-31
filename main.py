a256b28b171298b971d4d22b7ec091618e9ebcc3
kingjapo13 <kingjapo13@gmail.com>
2026-01-31 11:43:42 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 app = FastAPI()
+
 @app.post("/upload-video")
 async def upload_video(file: UploadFile = File(...)):
     file_path = os.path.join(UPLOAD_DIR, file.filename)
